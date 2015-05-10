c31279889efd803f459da5710eafe37f72de87c3
Robert Smiles Jr <bsmilesjr@yahoo.com>
2015-05-09 22:33:08 -0400

    program works for human v human and distinguishes between X and O but no ending yet, plus repeats you win

--- may7/tic-tac-toe.rb
+++ may7/tic-tac-toe.rb
@@ -1,26 +1,30 @@
 require 'pry'
 require 'set'
 
-def human_turn (all_selections, turn_tracker)
+def human_turn (selections_of_player_X, selections_of_player_O, turn_tracker)
   if turn_tracker.odd?; x="X" else x="O" end
 	puts "your turn player #{x}"
 	puts "select your space by typing 1-9"
 	selection = gets.chomp.to_i
-	until valid_selection(all_selections, selection)
+	until valid_selection(selections_of_player_X, selections_of_player_O, selection)
 		puts "that space is either already taken or outside the range of the board"
 		puts "make another selection:"
 		selection = gets.chomp.to_i
 	end
-	all_selections.add(selection)
-  if x=="X"; selections_of_player_X(selection) end
-  if x=="O"; selections_of_player_O(selection) end 
-	puts "at the end of human turn #{all_selections.to_a}"
+	#all_selections.add(selection)
 	selection
 end
 
-def valid_selection (all_selections, selection)
+def keep_track_of_selections_of_player_X (selection,selections_of_player_X)
+  selections_of_player_X.add(selection)
+end
+def keep_track_of_selections_of_player_O (selection,selections_of_player_O)
+  selections_of_player_O.add(selection)
+end
+
+def valid_selection (selections_of_player_X, selections_of_player_O, selection)
 	number_range = (1..9).include?(selection)
-	already_selected = !all_selections.include?(selection)
+	already_selected = !selections_of_player_X.include?(selection) || !selections_of_player_O.include?(selection) 
 	number_range && already_selected
 end
 
@@ -50,24 +54,30 @@ def update_board (selection, board_array, turn_tracker)
       board_array[2][selection-7] = "0"
     end
   end
+  #print board_array[0][0..2]
+  #puts
+  #print board_array[1][0..2]
+  #puts
+  #print board_array[2][0..2]
+  #puts
 end
 
 def display_board (board_array)	
 	print board_array[0][0..2]
-	puts
+	puts#
 	print board_array[1][0..2]
 	puts
 	print board_array[2][0..2]
 	puts
-	x = "o"
-	puts "#{x}|#{x}|#{x} "
-	puts "-+-+-"
-	puts "#{x}|#{x}|#{x}"
-	puts "-+-+-"
-	puts "#{x}|#{x}|#{x}"
+	#x = "-"
+	#puts "#{x}|#{x}|#{x} "
+	#puts "-+-+-"
+	#puts "#{x}|#{x}|#{x}"
+	#puts "-+-+-"
+	#suts "#{x}|#{x}|#{x}"
 end
 
-def did_somebody_win(all_selections) 
+def did_somebody_win(selections_of_player_X, selections_of_player_O) 
   #puts "TEST: at somebody win #{all_selections.to_a}"
   #choices = all_selections.to_set
   win1 = Set.new [1,2,3]
@@ -78,9 +88,14 @@ def did_somebody_win(all_selections)
   win6 = Set.new [3,6,9]
   win7 = Set.new [4,5,6]
   win8 = Set.new [7,8,9]
-  if (win1.subset?(all_selections.to_set) ||  win2.subset?(all_selections.to_set) ||  win3.subset?(all_selections.to_set) ||
-      win4.subset?(all_selections.to_set) || win5.subset?(all_selections.to_set) || win6.subset?(all_selections.to_set) ||
-      win7.subset?(all_selections.to_set) || win8.subset?(all_selections.to_set))
+  if (win1.subset?(selections_of_player_X.to_set) ||  win2.subset?(selections_of_player_X.to_set) ||  win3.subset?(selections_of_player_X.to_set) ||
+      win4.subset?(selections_of_player_X.to_set) || win5.subset?(selections_of_player_X.to_set) || win6.subset?(selections_of_player_X.to_set) ||
+      win7.subset?(selections_of_player_X.to_set) || win8.subset?(selections_of_player_X.to_set))
+      puts "you win"
+  end 
+    if (win1.subset?(selections_of_player_O.to_set) ||  win2.subset?(selections_of_player_O.to_set) ||  win3.subset?(selections_of_player_O.to_set) ||
+      win4.subset?(selections_of_player_O.to_set) || win5.subset?(selections_of_player_O.to_set) || win6.subset?(selections_of_player_O.to_set) ||
+      win7.subset?(selections_of_player_O.to_set) || win8.subset?(selections_of_player_O.to_set))
       puts "you win"
   end   
 end
@@ -94,30 +109,38 @@ end
 
 def play_hangman (mode)
 	board_array =[[0,0,0],[0,0,0],[0,0,0]]
-	all_selections = Set.new
+	#all_selections = Set.new
+  selections_of_player_X = Set.new
+  selections_of_player_O = Set.new
   turn_tracker = 0
   x=0
+  display_board(board_array)
   until x==10
     if mode ==1
       turn_tracker +=1
-      selection = human_turn(all_selections, turn_tracker)
+      selection = human_turn(selections_of_player_X, selections_of_player_O, turn_tracker)
     elsif mode ==2
       turn_tracker += 1
       if turn_tracker.even? 
-        selection = human_turn(all_selections)
+        selection = human_turn(selections_of_player_X, selections_of_player_O)
       else
-        selection = computer_turn(all_selections)
+        selection = computer_turn(selections_of_player_X, selections_of_player_O)
       end
     elsif mode ==3
       turn_tracker +=1
-      selection = computer_turn(all_selections)
+      selection = computer_turn(selections_of_player_X, selections_of_player_O)
     else
       puts "you didn't select a proper mode"
     end
 		#puts "passed point 1"
+    if turn_tracker.odd?
+      selections_of_player_X = keep_track_of_selections_of_player_X(selection,selections_of_player_X)
+    else
+      selections_of_player_X = keep_track_of_selections_of_player_O(selection,selections_of_player_O)
+    end
 		update_board(selection, board_array, turn_tracker)
 		display_board(board_array)
-    did_somebody_win(all_selections)
+    did_somebody_win(selections_of_player_X, selections_of_player_O)
     x +=1
 	end
 end

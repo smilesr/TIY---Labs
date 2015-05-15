7a086aba893da13662e8666031dd09079e23d4e7
Robert Smiles Jr <bsmilesjr@yahoo.com>
2015-05-15 12:57:52 -0400

    initial code for board class, but without winner determination

--- may15/ttt-board.rb
+++ may15/ttt-board.rb
@@ -1,19 +1,36 @@
+require 'pry'
+
 class Board
 
-  def board_status
+  WINNING_COMBOS = ()
+
+  def initialize
+    @board = board
+      @board = (1..9).to_a
   end
 
+
+  def board_status(selection, letter)
+    @board[selection] = letter  
+  end
+
+  def board_display
+    puts" #{board[0]}     |     #{board[1]}     |     #{board[2]}
+             ---------          -----------           -----------
+          #{board[3]}     |     #{board[4]}     |     #{board[5]}
+            ----------          -----------           -----------
+          #{board[6]}     |     #{board[7]}     |     #{board[8]}"
+  end
   def win?
   end
 
-  def taken_spaces
-
+  def available
+    @board.each {|x| x.to_i}
+    @board = @board.find_all {|y| y>0}
   end
 
-  def range_check
-  end
 
-  
+
 
 
 end 

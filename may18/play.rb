c88d78197dc701e38d3841153b1e660f99576ab0
Robert Smiles Jr <bsmilesjr@yahoo.com>
2015-05-20 14:02:14 -0400

    The play again option is now functional.  Still missing testing.

--- may18/play.rb
+++ may18/play.rb
@@ -6,35 +6,51 @@ require './tictactoe'
 # x = String.new
 # name = String.new
 # designation = String.new
-overall_game_tracker = 1
-while overall_game_tracker == 1
+#overall_game_tracker = 1
+#while overall_game_tracker == 1
 puts "You are playing tic tac toe."
 puts ""
-puts "Is first player human or computer:"
-x = gets.chomp.capitalize
-# puts "Name of first player (can hit <enter> if computer):"
-# name = gets.chomp
-#puts "Will the first player be 'x' or 'o':"
-designation = 'X'
+game_tracker = 1
+while game_tracker ==1
+  puts "Is first player human or computer:"
+  x = gets.chomp.capitalize
 
-if x == "Human"
-  player1 = Human.new(designation)
-elsif x == "Computer"
-  player1 = Computer.new(designation)
-end
+  designation = 'X'
 
-puts "Is second player human or computer:"
-x = gets.chomp.capitalize
-# puts "Name of second player (can hit <enter> if computer):"
-# name = gets.chomp
-designation = 'O'
+  if x == "Human"
+    player1 = Human.new(designation)
+  elsif x == "Computer"
+    player1 = Computer.new(designation)
+  end
 
-if x == "Human"
-  player2 = Human.new(designation)
-elsif x == "Computer"
-  player2 = Computer.new(designation)
-end
+  puts "Is second player human or computer:"
+  x = gets.chomp.capitalize
 
-game_time = TicTacToe.new(player1, player2, overall_game_tracker)
-overall_game_tracker = game_time.play
-end
+  designation = 'O'
+
+  if x == "Human"
+    player2 = Human.new(designation)
+  elsif x == "Computer"
+    player2 = Computer.new(designation)
+  end
+
+  game_time = TicTacToe.new(player1, player2)
+  game_time.play
+  puts "Do you want to play again (1) yes or (2) no?"
+  result = gets.chomp
+  until result =~ /^[12]$/
+    puts "Do you want to play again (1) yes or (2) no; enter 1 or 2?"
+    result = gets.chomp
+  end
+  game_tracker = result.to_i
+  if game_tracker == 1
+    puts "OK. Let's play again"
+    puts ''
+  else
+    puts "Thanks for playing!"
+  end
+end 
+  #  , overall_game_tracker)
+#if game_time(overall_game_tracker) == 1
+
+#end
--- may18/tictactoe.rb
+++ may18/tictactoe.rb
@@ -7,12 +7,12 @@ class TicTacToe
 
   attr_writer :board
 
-  def initialize(player1, player2, overall_game_tracker)
+  def initialize(player1, player2)#, overall_game_tracker)
     @board = Board.new
     # @play = play
     @player1 = player1
     @player2 = player2
-    @overall_game_tracker = overall_game_tracker
+#    @overall_game_tracker = overall_game_tracker
   end
 
 
@@ -34,7 +34,7 @@ class TicTacToe
     end
 
     if @board.win?
-      # Due to the structure of the code, the current player advances to the next player after a winner is discovered,
+      # Due to the structure of the code, the current player advances to the next player AFTER a winner is discovered,
       # so following code corrects this anomaly for purposes of identifying the winner
       if current_player.designation == "O"
         current_player.designation = "X"
@@ -45,7 +45,9 @@ class TicTacToe
     else
       puts "It was a Cat's Game!"
     end
-  puts "Do you want to play again: (1) yes or (2) no"
-  @overall_game_tracker = gets.chomp.to_i
+  # puts "Do you want to play again: (1) yes or (2) no"
+  # @overall_game_tracker = gets.chomp.to_i
+  # game_time(@overall_game_tracker)
+
   end
 end
